346c63ce3c9d29165991d0f918583250ac5c4562
Wussypants <jacob.molin@me.com>
2016-02-08 23:11:33 +0100

    Initial Commit

---
+++ 04 MyFirstBrower/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  04 MyFirstBrower
+//
+//  Created by Jacob Molin Nielsen on 08/02/2016.
+//  Copyright © 2016 Wussypants. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
